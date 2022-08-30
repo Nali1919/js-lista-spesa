43862455d7d0baecffb816de7d046a7ef72cdc81
Nali1919 <my@email.com>
2022-08-30 15:07:15 +0200

    Creo variabile while

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log('Js is ok')
+
+let listaSpesa = ['Pane', 'Pomodori', 'Insalata', 'Prosciutto']
+
+
+let i = 0
+
+while (i < listaSpesa.length) {
+    console.log(listaSpesa[i])
+
+    i++ 
+    
+}
