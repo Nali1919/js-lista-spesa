7cee6e59ee943ed03af8e1f30779b5ced61268ad
Nali1919 <my@email.com>
2022-08-31 19:47:39 +0200

    Modifiche

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@ console.log('Js is ok')
 
 let listaSpesa = ['Pane', 'Pomodori', 'Insalata', 'Prosciutto']
 
+let lista = document.getElementById('spesa')
 
 let i = 0
 
+
+
 while (i < listaSpesa.length) {
     console.log(listaSpesa[i])
-
+    
+    lista.innerHTML= (listaSpesa)
+    
+   
     i++ 
     
 }
